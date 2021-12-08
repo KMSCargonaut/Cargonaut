018c85d3fbd7782ea94f99b9e24da6adbadd73e8
Niklas Thy <niklas.thy.nt@gmail.com>
2021-12-08 18:15:10 +0100

    added firebaseConfig in environment.prod for building in CI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAzFvzwYxlMK_XDHfw1FZj2KdVqf0CXleA",
+    authDomain: "kmsws21-cargonaut.firebaseapp.com",
+    projectId: "kmsws21-cargonaut",
+    storageBucket: "kmsws21-cargonaut.appspot.com",
+    messagingSenderId: "1019573337686",
+    appId: "1:1019573337686:web:6c95b8ee68daf7613cc5f7",
+    measurementId: "${config.measurementId}"
+  }
 };
