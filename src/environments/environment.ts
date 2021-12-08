b9dd31ced6862983a28e2a48e7c541be6435fdc8
Niklas Thy <niklas.thy.nt@gmail.com>
2021-12-08 18:10:27 +0100

    added firebase to project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAzFvzwYxlMK_XDHfw1FZj2KdVqf0CXleA",
+    authDomain: "kmsws21-cargonaut.firebaseapp.com",
+    projectId: "kmsws21-cargonaut",
+    storageBucket: "kmsws21-cargonaut.appspot.com",
+    messagingSenderId: "1019573337686",
+    appId: "1:1019573337686:web:6c95b8ee68daf7613cc5f7",
+    measurementId: "${config.measurementId}"
+  }
 };
 
 /*
