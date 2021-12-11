24a8ac53c1728fb6ce0332f1df2dc1a5ee5fdfa5
Serageldin <serageldin.el-khodary@mni.thm.de>
2021-12-11 10:35:35 +0100

    Added missing files from suchleiste

---
+++ src/app/suchleiste/suchleiste.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SuchleisteComponent } from './suchleiste.component';
+
+describe('SuchleisteComponent', () => {
+  let component: SuchleisteComponent;
+  let fixture: ComponentFixture<SuchleisteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SuchleisteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SuchleisteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/suchleiste/suchleiste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suchleiste',
+  templateUrl: './suchleiste.component.html',
+  styleUrls: ['./suchleiste.component.css']
+})
+export class SuchleisteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
