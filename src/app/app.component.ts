1bccf244d42939ce43247130721c0d93bd2fc798
Niklas Thy <niklas.thy.nt@gmail.com>
2021-12-13 17:37:51 +0100

    changed infoCard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cargonaut';
+
+  boxTitles = [
+    "Lass dich fahren für weniger Geld",
+    "Dein Transport: Einfach und unkompliziert",
+    "Sorglos fahren, dank Nutzerbewertungen"
+  ]
+
+  infoTexts = [
+    "Hier steht ein Beispieltext zum anlocken von neuen Nutzern durch unser Feature, dass es relativ günstig ist, mit uns zu fahren!",
+    "Hier steht ein Beispieltext zum anlocken von neuen Nutzern durch unser Feature, dass es einfach und unkompliziert ist, mit uns zu fahren!",
+    "Hier steht ein Beispieltext zum anlocken von neuen Nutzern durch unser Feature, dass es sorglos, durch Nutzerbewertung, ist, mit uns zu fahren!"
+  ]
 }
--- src/app/infobox/infobox.component.ts
+++ src/app/infobox/infobox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-infobox',
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoboxComponent{
 
+  @Input() boxTitle = '';
+  @Input() infoText = '';
+  @Input() imagePath = '';
   constructor() { }
-
-
 
 }
