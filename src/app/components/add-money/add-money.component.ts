3d1fb605eef6d67c40f865306b7f2757ee233325
Niklas Thy <niklas.thy.nt@gmail.com>
2021-12-20 16:14:57 +0100

    Initial Commit for this branch

---
+++ src/app/components/add-money/add-money.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-money',
+  templateUrl: './add-money.component.html',
+  styleUrls: ['./add-money.component.css']
+})
+export class AddMoneyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
