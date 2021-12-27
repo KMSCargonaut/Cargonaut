95a60f5134e09c832bc9c03edc92a22ad4536ef7
TheSoniix <70700254+TheSoniix@users.noreply.github.com>
2021-12-27 16:00:05 +0100

    added two parallel lists

--- src/app/components/tour-site/tour-site.component.ts
+++ src/app/components/tour-site/tour-site.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Tour} from "../../models/Tour";
 import {TourService} from "../../services/tour.service";
+import {elementAt} from "rxjs/operators";
 
 @Component({
   selector: 'app-tour-site',
@@ -9,28 +10,52 @@ import {TourService} from "../../services/tour.service";
 })
 
 //TODO Niklas Thy - Lesbarkeit verbessern
-export class TourSiteComponent implements OnInit{
+export class TourSiteComponent implements OnInit {
 
   searchbarheigth = "5em";
-  isOffer: boolean = false;
+  isOffer: boolean = true;
   offerTours: Tour[] = [];
   requestTours: Tour[] = []
   isHorizontal: boolean = false;
+  rightTours: Tour[] = []
+  leftTours: Tour[] = [];
 
-  constructor(public tourService: TourService) { }
+  constructor(public tourService: TourService) {
+  }
 
   offerOnOff() {
     this.isOffer = !this.isOffer;
-    console.log(this.isOffer);
+    if (this.isOffer) {
+      this.splitList(this.offerTours);
+    } else {
+      this.splitList(this.requestTours);
+    }
   }
 
   ngOnInit() {
     this.setTours().then();
   }
 
-  async setTours(){
+  async setTours() {
     this.offerTours = await this.tourService.getAllTours().then();
     this.requestTours = this.offerTours.filter(tour => !tour.offer);
     this.offerTours = this.offerTours.filter(tour => tour.offer);
   }
+
+  splitList(tours: Tour[]) {
+    let right: Tour[] = [];
+    let left: Tour [] = [];
+    for (let i = 0; i < tours.length; i++) {
+      let index: number = i + 1;
+      if (index % 2 === 0) {
+        right.push(tours[i]);
+      } else {
+        left.push(tours[i])
+      }
+    }
+    this.rightTours = [...right];
+    this.leftTours = [...left];
+  }
+
+
 }
