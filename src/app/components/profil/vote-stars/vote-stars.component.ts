bda77bb0bf99795a811adf065fde284b89c0c447
TheSoniix <70700254+TheSoniix@users.noreply.github.com>
2021-12-28 03:05:42 +0100

    fixed code smell

--- src/app/components/profil/vote-stars/vote-stars.component.ts
+++ src/app/components/profil/vote-stars/vote-stars.component.ts
@@ -1,15 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-vote-stars',
   templateUrl: './vote-stars.component.html',
   styleUrls: ['./vote-stars.component.css']
 })
-export class VoteStarsComponent implements OnInit {
+export class VoteStarsComponent {
 @Input() currentRate: number = 0;
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
