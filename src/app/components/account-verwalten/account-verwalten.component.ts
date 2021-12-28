a64c4e565f8a9383556f45bd5557b8f517601d91
Loic Maxwell Ngouabe <loic.maxwell.tchiabe.ngouabe@mni.thm.de>
2021-12-28 16:36:41 +0100

    component erstellen

---
+++ src/app/components/account-verwalten/account-verwalten.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-verwalten',
+  templateUrl: './account-verwalten.component.html',
+  styleUrls: ['./account-verwalten.component.css']
+})
+export class AccountVerwaltenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
