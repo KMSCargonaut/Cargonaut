0c219811d806a56b2732a0dc9cd58cf6aa6b673f
Loic Maxwell Ngouabe <loic.maxwell.tchiabe.ngouabe@mni.thm.de>
2021-12-28 17:11:18 +0100

    neu component erstellen

---
+++ src/app/components/edit-account/edit-account.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-account',
+  templateUrl: './edit-account.component.html',
+  styleUrls: ['./edit-account.component.css']
+})
+export class EditAccountComponent  {
+
+  constructor() { }
+
+
+
+}
