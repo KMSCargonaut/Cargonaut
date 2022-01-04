770890c4fbebb5dfa487e8fc6e750b5cc0503948
Loic Maxwell Ngouabe <loic.maxwell.tchiabe.ngouabe@mni.thm.de>
2022-01-05 00:46:04 +0100

    update

--- src/app/components/carlist/car-item/car-item.component.ts
+++ src/app/components/carlist/car-item/car-item.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {UserService} from "../../../services/user.service";
 import {Car} from "../../../models/Car";
 import {CarsService} from "../../../services/cars.service";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-car-item',
@@ -24,79 +25,22 @@ export class CarItemComponent implements OnInit{
   }
 
 
-  //Checking Input
-  public model = this.car?.model ? this.car?.model: '';
-  public mark = this.car?.mark ? this.car?.mark  : '';
-  public seats = this.car?.seats ? this.car?.seats : '';
-  public storage= this.car?.storage ? this.car?.storage  : undefined;
 
-  // PropertyBinding for outline color for wrong/no inputs
-  public wrongFahrzeugModel = '';
-  public wrongKennzeichen = '';
-  public wrongSitzplaze = '';
-  public wrongStauraum = '';
+  public message = '';
 
 
-
-  // Message for wrong/no inputs
-  public fahrzeugModelMessage = '';
-  public kennzeichenMessage = '';
-  public sitzplazeMessage = '';
-  public stauraumMessage = '';
-
-
-  public async updateUser(){
-    if(this.carData){
-     // await this.carData.updateCar(this.car);
+  public async updateCar(){
+    if(this.car){
+      await this.carData.updateCar(this.car);
     }
+    this.message="Fahrzeug erfolgreich geändert"
 
   }
 
-  public async inputCheck() {
-    if (this.model.trim().length > 0 &&
-      this.mark.trim().length > 0 &&
-      this.seats > 0 ) {
-      await this.updateUser();
-    } else {
-      if (this.model.trim().length === 0) {
-        this.fahrzeugModelMessage = 'Geben Sie Ihren Vornamen an';
-        this.wrongFahrzeugModel = 'border-danger';
-      }
-      if (this.mark.trim().length === 0) {
-        this.kennzeichenMessage = 'Geben Sie Ihren Nachnamen an';
-        this.wrongKennzeichen = 'border-danger';
-      }
-
-    }
-  }
 
 
-  public validFahrzeugModel(input: string): void {
-    if (input.trim().length >= 0) {
-      this.fahrzeugModelMessage = '';
-      this.wrongFahrzeugModel = '';
-    }
-  }
 
-  public validKennzeichen(input: string): void {
-    if (input.trim().length >= 0) {
-      this.kennzeichenMessage = '';
-      this.wrongKennzeichen = '';
-    }
-  }
 
-  public validSitzplaze(input: string): void {
-    if (input.trim().length >= 0) {
-      this.sitzplazeMessage = '';
-      this.wrongSitzplaze = '';
-    }
-  }
-  public validStauraum(input: string): void {
-    if (input.trim().length >= 0) {
-      this.sitzplazeMessage = '';
-      this.wrongSitzplaze = '';
-    }
-  }
 
 
 }
