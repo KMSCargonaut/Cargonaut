0a827be54f8569e324912e338b434beaf2fb6ea0
TheSoniix <70700254+TheSoniix@users.noreply.github.com>
2022-01-06 17:02:04 +0100

    added purchase

--- src/app/components/tour-confirm/tour-confirm.component.ts
+++ src/app/components/tour-confirm/tour-confirm.component.ts
@@ -3,13 +3,16 @@ import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {ShareDataService} from "../../services/share-data.service";
 import {ConfirmService} from "../../services/confirm.service";
 import {Confirm} from "../../models/Confirm";
+import {Tour} from "../../models/Tour";
+import {UserService} from "../../services/user.service";
+import {AlertService} from "../../services/alert.service";
 
 @Component({
   selector: 'app-tour-confirm',
   templateUrl: './tour-confirm.component.html',
   styleUrls: ['./tour-confirm.component.css']
 })
-export class TourConfirmComponent implements OnInit{
+export class TourConfirmComponent implements OnInit {
 
 
   code: string = '';
@@ -23,11 +26,13 @@ export class TourConfirmComponent implements OnInit{
   pos7: string = '';
   pos8: string = '';
 
-  constructor(public activeModal: NgbActiveModal, public shareData: ShareDataService, public confirmData: ConfirmService) { }
+  constructor(public activeModal: NgbActiveModal, public shareData: ShareDataService, public confirmData: ConfirmService,
+              public userData: UserService, public alertData: AlertService) {
+  }
 
   async ngOnInit() {
     const tour = this.shareData.confirmTour
-    if ( tour && tour.dID) {
+    if (tour && tour.dID) {
       const confirm = await this.confirmData.findConfirmationByTourId(tour.dID)
       if (confirm && confirm.length != 0) {
         this.alreadyCreated = true;
@@ -46,10 +51,10 @@ export class TourConfirmComponent implements OnInit{
   }
 
   makeid(length: number) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
+    for (var i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() *
         charactersLength));
     }
@@ -58,30 +63,59 @@ export class TourConfirmComponent implements OnInit{
 
   async confirmCode() {
     const tour = this.shareData.confirmTour
-    if ( tour && tour.dID) {
+    if (tour && tour.dID) {
       const confirm = await this.confirmData.findConfirmationByTourId(tour.dID)
       if (confirm && confirm.length != 0) {
-       const inputCode = this.pos1 + this.pos2 + this.pos3 + this.pos4 + this.pos5 + this.pos6 + this.pos7 + this.pos8;
-       if(confirm[0].code === inputCode) {
-         //Todo: Buchung
-       } else {
-         // Todo: Code nicht korrekt
-       }
+        const inputCode = this.pos1 + this.pos2 + this.pos3 + this.pos4 + this.pos5 + this.pos6 + this.pos7 + this.pos8;
+        if (confirm[0].code === inputCode) {
+          await this.purchase(tour)
+        } else {
+          // TODO: input färben
+          this.alertData.showAlert({type: 'danger', message: 'Code ist nicht richtig!'})
+        }
       }
     }
   }
 
- async createCode() {
+  async createCode() {
     const tour = this.shareData.confirmTour;
-    if(tour && tour.dID) {
+    if (tour && tour.dID) {
       await this.randomizedCode();
       await this.confirmData.addConfirm(new Confirm(tour.dID, this.code))
       this.alreadyCreated = true;
     }
   }
 
-  async purchase() {
+  async purchase(tour: Tour) {
+    if (this.userData.currUser) {
+      const currPassenger = tour.passengers.find(passenger => passenger.id === this.userData.currUser?.uid);
+      if (currPassenger) {
+        const currUser = await this.userData.getUser(currPassenger.id);
 
+        if (currUser) {
+          const costs = (currPassenger.seats + currPassenger.storage) * tour.price;
+          const credit = currUser.money - costs;
+          if (credit < 0) {
+            this.alertData.showAlert({
+              type: 'danger',
+              message: 'Bezahlung war nicht erfolgreich! Laden Sie Ihr Guthaben auf!'
+            })
+        } else {
+            const driver = await this.userData.getUser(tour.driver);
+            console.log(driver)
+            console.log(credit)
+            console.log(currUser);
+            if (driver) {
+              currUser.money = credit;
+              driver.money += costs;
+              await this.userData.updateUser(currUser)
+              await this.userData.updateUser(driver)
+              this.alertData.showAlert({type: 'success', message: 'Bezahlung war erfolgreich!'})
+            }
+          }
+        }
+      }
+    }
   }
 
 }
