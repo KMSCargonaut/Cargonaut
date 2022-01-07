2fbe3a01c795ab063bafd88bef68bc1591643065
Niklas Thy <niklas.thy.nt@gmail.com>
2022-01-07 16:12:09 +0100

    addes two components for posts, not finished but has basic html

---
+++ src/app/components/tour-details/post-list/post-card/post-card.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-post-card',
+  templateUrl: './post-card.component.html',
+  styleUrls: ['./post-card.component.css']
+})
+export class PostCardComponent{
+
+  constructor() { }
+
+}
---
+++ src/app/components/tour-details/post-list/post-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent{
+
+  constructor() { }
+
+
+}
